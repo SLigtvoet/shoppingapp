0c9a5d17ccb2e3ab654131080428140cde74cc36
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-08-03 14:26:00 +0200

    first try

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var Sequelize = require('sequelize')
-var sequelize = new Sequelize('postgres://dcvrqnrfogtpon:39514c5bb51a47239f5500265205a3eebbc5c5b220981ac1728d47acfb4bd054@ec2-54-217-214-201.eu-west-1.compute.amazonaws.com:5432/db84p8pgqbv9gu')
+var sequelize = new Sequelize('postgres://mgtcoycausaecg:7887ad45bc561ea4943a4fb988204f105cff6d9a86f6bcde164e408108d05c83@ec2-54-217-218-80.eu-west-1.compute.amazonaws.com:5432/d55jumfo4phqd9')
 
 module.exports = sequelize
